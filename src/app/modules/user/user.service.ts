8b5a0c4a498594f6417eda4b0a3f1c4b2748b35b
Al-Amin <alamin50cse.miu@gmail.com>
2025-08-14 04:28:23 +0600

    subscripton cheke expiry add

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,175 +1,85 @@
 import { StatusCodes } from "http-status-codes";
-import { JwtPayload } from "jsonwebtoken";
-import { USER_ROLES } from "../../../enums/user";
-import { emailHelper } from "../../../helpers/emailHelper";
-import { emailTemplate } from "../../../shared/emailTemplate";
-import unlinkFile from "../../../shared/unlinkFile";
-import { IUser } from "./user.interface";
 import { User } from "./user.model";
+import { IUser } from "./user.interface";
 import AppError from "../../../errors/AppError";
-import generateOTP from "../../../utils/generateOTP";
-// create user
-const createUserToDB = async (payload: IUser): Promise<IUser> => {
-  //set role
-  const user = await User.isExistUserByEmail(payload.email);
-  if (user) {
-    throw new AppError(StatusCodes.CONFLICT, "Email already exists");
-  }
-  payload.role = USER_ROLES.USER;
-  const createUser = await User.create(payload);
-  if (!createUser) {
-    throw new AppError(StatusCodes.BAD_REQUEST, "Failed to create user");
-  }
+import { JwtPayload } from "jsonwebtoken";
+import { unlinkFile } from "../../../shared/unlinkFile"; // adjust your path
+import bcrypt from "bcrypt";
 
-  //send email
-  const otp = generateOTP(4);
-  const values = {
-    name: createUser.name,
-    otp: otp,
-    email: createUser.email!,
-  };
-  const createAccountTemplate = emailTemplate.createAccount(values);
-  emailHelper.sendEmail(createAccountTemplate);
-
-  //save to DB
-  const authentication = {
-    oneTimeCode: otp,
-    expireAt: new Date(Date.now() + 3 * 60000),
-  };
-  await User.findOneAndUpdate(
-    { _id: createUser._id },
-    { $set: { authentication } }
-  );
-
-  return createUser;
+const createUserToDB = async (payload: Partial<IUser>): Promise<IUser> => {
+  const newUser = await User.create(payload);
+  return newUser;
 };
 
-// create Businessman
-const createVendorToDB = async (payload: IUser): Promise<IUser> => {
-  //set role
-  payload.role = USER_ROLES.HOSPITALITY_VENUE;
-  const createUser = await User.create(payload);
-  if (!createUser) {
-    throw new AppError(StatusCodes.BAD_REQUEST, "Failed to create user");
-  }
-
-  //send email
-  const otp = generateOTP(4);
-  const values = {
-    name: createUser.name,
-    otp: otp,
-    email: createUser.email!,
-  };
-  const createAccountTemplate = emailTemplate.createAccount(values);
-  emailHelper.sendEmail(createAccountTemplate);
-
-  //save to DB
-  const authentication = {
-    oneTimeCode: otp,
-    expireAt: new Date(Date.now() + 3 * 60000),
-  };
-  await User.findOneAndUpdate(
-    { _id: createUser._id },
-    { $set: { authentication } }
-  );
-
-  return createUser;
-};
-// create Admin
-// const createAdminToDB = async (
-//   payload: Partial<IUser>
-// ): Promise<IUser> => {
-//   //set role
-//   payload.role = USER_ROLES.ADMIN;
-//   const createAdmin = await User.create(payload);
-//   if (!createAdmin) {
-//     throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to create admin');
-//   }
-
-//   //send email
-//   const otp = generateOTP(6);
-//   const values = {
-//     name: createAdmin.name,
-//     otp: otp,
-//     email: createAdmin.email!,
-//   };
-//   const createAccountTemplate = emailTemplate.createAccount(values);
-//   emailHelper.sendEmail(createAccountTemplate);
-
-//   //save to DB
-//   const authentication = {
-//     oneTimeCode: otp,
-//     expireAt: new Date(Date.now() + 3 * 60000),
-//   };
-//   await User.findOneAndUpdate(
-//     { _id: createAdmin._id },
-//     { $set: { authentication } }
-//   );
-
-//   return createAdmin;
-// };
-
-// get user profile
+// ---------------- GET USER PROFILE ----------------
 const getUserProfileFromDB = async (
   user: JwtPayload
 ): Promise<Partial<IUser>> => {
-  const { id } = user;
-  const isExistUser = await User.isExistUserById(id);
-  if (!isExistUser) {
-    throw new AppError(StatusCodes.BAD_REQUEST, "User doesn't exist!");
-  }
+  const id = user.id;
+  if (!id) throw new AppError(StatusCodes.UNAUTHORIZED, "User ID missing in token");
+
+  const isExistUser: IUser | null = await User.isExistUserById(id);
+  if (!isExistUser) throw new AppError(StatusCodes.BAD_REQUEST, "User doesn't exist");
 
   return isExistUser;
 };
 
-// update user profile
+// ---------------- UPDATE PROFILE ----------------
 const updateProfileToDB = async (
   user: JwtPayload,
   payload: Partial<IUser>
 ): Promise<Partial<IUser | null>> => {
-  const { id } = user;
-  const isExistUser = await User.isExistUserById(id);
-  if (!isExistUser) {
-    throw new AppError(StatusCodes.BAD_REQUEST, "User doesn't exist!");
-  }
+  const id = user.id;
+  if (!id) throw new AppError(StatusCodes.UNAUTHORIZED, "User ID missing in token");
 
-  //unlink file here
-  if (payload.image) {
+  const isExistUser: IUser | null = await User.isExistUserById(id);
+  if (!isExistUser) throw new AppError(StatusCodes.BAD_REQUEST, "User doesn't exist");
+
+  // Unlink old image if new image provided
+  if (payload.image && isExistUser.image) {
     unlinkFile(isExistUser.image);
   }
 
-  const updateDoc = await User.findOneAndUpdate({ _id: id }, payload, {
+  const updatedUser: IUser | null = await User.findOneAndUpdate({ _id: id }, payload, {
     new: true,
   });
 
-  return updateDoc;
+  return updatedUser;
 };
 
-const verifyUserPassword = async (userId: string, password: string) => {
-  const user = await User.findById(userId).select("+password");
-  if (!user) {
-    throw new AppError(StatusCodes.NOT_FOUND, "User not found.");
-  }
+// ---------------- VERIFY PASSWORD ----------------
+const verifyUserPassword = async (userId: string, password: string): Promise<boolean> => {
+  if (!userId) throw new AppError(StatusCodes.UNAUTHORIZED, "User ID required");
+
+  const user: IUser | null = await User.findById(userId).select("+password");
+  if (!user) throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+
   const isPasswordValid = await User.isMatchPassword(password, user.password);
   return isPasswordValid;
 };
-const deleteUser = async (id: string) => {
-  const isExistUser = await User.isExistUserById(id);
-  if (!isExistUser) {
-    throw new AppError(StatusCodes.BAD_REQUEST, "User doesn't exist!");
-  }
 
-  await User.findByIdAndUpdate(id, {
-    $set: { isDeleted: true },
-  });
+// ---------------- DELETE USER ----------------
+const deleteUser = async (userId: string): Promise<boolean> => {
+  if (!userId) throw new AppError(StatusCodes.UNAUTHORIZED, "User ID required");
 
+  const isExistUser: IUser | null = await User.isExistUserById(userId);
+  if (!isExistUser) throw new AppError(StatusCodes.BAD_REQUEST, "User doesn't exist");
+
+  await User.findByIdAndUpdate(userId, { $set: { isDeleted: true } });
   return true;
 };
+
+// ---------------- CREATE VENDOR (example) ----------------
+const createVendorToDB = async (payload: Partial<IUser>): Promise<IUser> => {
+  const newVendor = await User.create(payload);
+  return newVendor;
+};
+
 export const UserService = {
   createUserToDB,
   getUserProfileFromDB,
   updateProfileToDB,
-  createVendorToDB,
-  deleteUser,
   verifyUserPassword,
+  deleteUser,
+  createVendorToDB,
 };
