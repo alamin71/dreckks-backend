315f5fa7daa29fe06bf489f391ea266aebc38164
Al-Amin <alamin50cse.miu@gmail.com>
2025-08-17 23:30:51 +0600

    aadd

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,22 +1,6 @@
 
 
-// import express from 'express';
-// import * as AuthController from './auth.controller';
-// import { AuthValidation } from './auth.validation';
-// import validateRequest from '../../middleware/validateRequest'; 
 
-// const router = express.Router();
-
-// router.post('/signup', validateRequest(AuthValidation.createSignupZodSchema), AuthController.signup);
-// router.post('/verify-otp', validateRequest(AuthValidation.createVerifyOtpZodSchema), AuthController.verifyOtp);
-// router.post('/login', validateRequest(AuthValidation.createLoginZodSchema), AuthController.login);
-// router.post('/refresh-token', validateRequest(AuthValidation.createRefreshTokenZodSchema), AuthController.refreshToken);
-// router.post('/resend-otp', validateRequest(AuthValidation.createResendOtpZodSchema), AuthController.resendOtp);
-// router.post('/forgot-password', validateRequest(AuthValidation.createForgotPasswordZodSchema), AuthController.forgotPassword);
-// router.patch('/reset-password', validateRequest(AuthValidation.createResetPasswordZodSchema), AuthController.resetPassword);
-// router.post('/change-password', validateRequest(AuthValidation.createChangePasswordZodSchema), AuthController.changePassword);
-
-// export default router;
 import express from 'express';
 import * as AuthController from './auth.controller';
 import { AuthValidation } from './auth.validation';
@@ -37,6 +21,6 @@ router.post('/refresh-token', validateRequest(AuthValidation.createRefreshTokenZ
 router.post('/forgot-password', validateRequest(AuthValidation.createForgotPasswordZodSchema), AuthController.forgotPasswordController);
 router.post('/verify-forgot-password-otp', validateRequest(AuthValidation.verifyForgotPasswordOtpZodSchema), AuthController.verifyForgotPasswordOtpController);
 router.patch('/reset-password', validateRequest(AuthValidation.resetPasswordZodSchema), AuthController.resetPasswordController);
-router.post('/change-password', validateRequest(AuthValidation.createChangePasswordZodSchema), AuthController.changePasswordController);
+router.patch('/change-password', validateRequest(AuthValidation.createChangePasswordZodSchema), AuthController.changePasswordController);
 
 export default router;
